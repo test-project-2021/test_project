42e40e1cc20ed9813b8a67b714c07769ecab05a7
Roman Ibrahimov <ibrahir@purdue.edu>
2021-06-17 02:10:28 -0400

    testing TravisCI

---
+++ main.py
@@ -0,0 +1,9 @@
+from validator import Validator
+print("hi")
+username = 'Roman'
+validator = Validator()
+print("hi")
+if validator.username_is_valid(username):
+    print("username is valid")
+else:
+    print("username is not valid")
---
+++ tests/test_validator.py
@@ -0,0 +1,26 @@
+import unittest
+from validator import Validator
+
+class test_validator(unittest.TestCase):
+    def test_reject_user_name_if_long(self):
+        #Assume
+        username= 'InvalidTooLong'
+        validator = Validator()
+
+        #Action
+        result = validator.username_is_valid(username)
+
+        #Assert
+        self.assertFalse(result)
+
+    def  test_reject_user_name_if_space(self):
+        #Assume
+        username= 'R man'
+        validator = Validator()
+
+
+        #Action
+        result = validator.username_is_valid(username)
+
+        #Assert
+        self.assertFalse(result)
